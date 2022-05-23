41e3568a10502aa4d8ea75f42baa71947342395d
구나현 <gnh@gunahyeon-ui-MacBookPro.local>
2022-05-24 07:07:04 +0900

    폴더 구조 최적화

---
+++ app/src/routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.home); //ctrl.output.home 으로 원랜 명시해주는 것이 좋다.
+router.get("/login", ctrl.login);
+
+module.exports = router; //라우터를 외부파일에서 사용할 수 있게 내보내주기
