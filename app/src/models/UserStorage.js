801ad564ee6fe610a0a395beadb3886f044a0f0f
gunahyeon <rnskqkd@naver.com>
2022-05-25 07:03:12 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -3,25 +3,6 @@
 const fs = require("fs").promises;
 
 class UserStorage {
-    //외부에서 접근할 수 없게 #를 사용하여 데이터를 은닉화를 한다, 메서드로 접근해야한다. , 바로 지정해서 사용할 수 있게 static화한다.
-
-    // static getUsers(...fields) {
-    //     // getUsers 파람으로 (선택한)배열이 여러개 들어온다. ex: getUsers("id","psword")
-    //     const users = this.#users;
-    //     const newUsers = fields.reduce((newUsers, field)=>{
-    //         //새로운 users를 만들어보자
-    //         if (users.hasOwnProperty(field)) {
-    //             newUsers[field] = users[field];
-    //         }
-    //         return newUsers; //다른 파라미터의 newUsers로 들어가게 된다.
-    //     }, {});
-    //     console.log(newUsers);
-    //     //newUsers => id, field=> psword(반복문), 로 들어온다. 빈오브젝트는 newUsers를 가리킴.
-    //     return newUsers;
-
-    //     //내가 이해한 것 : 전체 오브젝트에서 새로운 오브젝트를 만들어 리턴하는 과정이다.
-    // }
-
     // 은닉화한 메서드는 항상 최상단에 올려주는게 좋다. 코딩문화.
     static #getUserInfo(data, id){
         const users = JSON.parse(data);
@@ -34,19 +15,61 @@ class UserStorage {
 
         return userInfo;
     }
-    static save(userInfo) {
-        // const users = this.#users;
+
+    static #getUsers(data, isAll , fields) {
+        //모든 객체들이 들어오면 바로 return 하겠다.
+        // getUsers 파람으로 (선택한)배열이 여러개 들어온다. ex: getUsers("id","psword")
+        const users = JSON.parse(data);
+        if(isAll) return users;
+        const newUsers = fields.reduce((newUsers, field)=>{
+            //새로운 users를 만들어보자
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers; //다른 파라미터의 newUsers로 들어가게 된다.
+        }, {});
+        //newUsers => id, field=> psword(반복문), 로 들어온다. 빈오브젝트는 newUsers를 가리킴.
+        return newUsers;
+
+        //내가 이해한 것 : 전체 오브젝트에서 새로운 오브젝트를 만들어 리턴하는 과정이다.
+    }
+
+    //외부에서 접근할 수 없게 #를 사용하여 데이터를 은닉화를 한다, 메서드로 접근해야한다. , 바로 지정해서 사용할 수 있게 static화한다.
+    static getUsers(isAll, ...fields) {
+        return fs
+        .readFile("./src/databases/users.json") 
+        .then((data)=>{
+            return this.#getUsers(data, isAll ,fields);
+        })
+        .catch(console.error);
     }
 
     //아이디 받아서 해당하는 유저 정보 던져주기
     static getUserInfo(id) {
         //promise를 반환하면 then, catch이라는 메서드를 사용할 수 있다.
-        return fs.readFile("./src/databases/users.json") 
+        return fs.
+        readFile("./src/databases/users.json") 
         .then((data)=>{
             //가독성을 위해 은닉화하여 분리
-            return this.#getUserInfo(data,id);
+            return this.#getUserInfo(data, id);
         })
         .catch(console.error);
+    }
+
+    static async save(userInfo) {
+        // const users = await this.getUsers("id","psword","username");
+        // 모든 데이터 값 가져오라는 의미는 true로 작성하면됑.
+        const users = await this.getUsers(true);
+        console.log(users);
+        if(users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디입니다.";
+        }
+        users.id.push(userInfo.id);
+        users.username.push(userInfo.username);
+        users.psword.push(userInfo.psword);
+        //데이터추가
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return {success : true};
     }
 }
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const id = document.querySelector("#id"); //선택자
-var username = document.querySelector("#name");
+var username = document.querySelector("#username");
 var psword = document.querySelector("#psword");
 var confirmPsword = document.querySelector("#confirm-psword");
 var registerBtn = document.querySelector("#button");
@@ -16,26 +16,25 @@ function register() {
 
     const req = {
         id : id.value,
-        name : username.value,
+        username : username.value,
         psword : psword.value,
     };
-    console.log(req);
 //라우터에게 요청한다.
-// fetch("/register", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type" : "application/json",
-//     },
-//     body: JSON.stringify(req),
-// }).then((res) => res.json()).
-//     then((res) => {
-//         if(res.success) {
-//             location.href="/";
-//         } else {
-//             alert(res.msg);
-//         }
-// })
-// .catch((err) => {
-//     console.log("로그인 중 에러 발생");
-// })
+fetch("/register", {
+    method: "POST",
+    headers: {
+        "Content-Type" : "application/json",
+    },
+    body: JSON.stringify(req),
+}).then((res) => res.json()).
+    then((res) => {
+        if(res.success) {
+            location.href="/login";
+        } else {
+            alert(res.msg);
+        }
+})
+.catch((err) => {
+    console.log("로그인 중 에러 발생");
+})
 }; //반환값이 Promise, .json()을 해줘야 다 읽는다.
