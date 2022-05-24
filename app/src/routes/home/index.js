64a54f7feaed908fb903e29e3bec4ee604e28590
gunahyeon <rnskqkd@naver.com>
2022-05-25 04:34:55 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home); //ctrl.output.home 으로 원랜 명시해주는 것이 좋다.
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router; //라우터를 외부파일에서 사용할 수 있게 내보내주기
